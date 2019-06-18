941f0c2885c097699b1a8060dcc52c4cf024516c
Louis <woodylouis@users.noreply.github.com>
2019-06-18 22:14:37 +1000

    CRUD

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,11 +3,35 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->notEmpty('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmpty('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
--- src/Template/Articles/index.ctp
+++ src/Template/Articles/index.ctp
@@ -1,10 +1,13 @@
 <!-- File: src/Template/Articles/index.ctp -->
 
 <h1>Articles</h1>
+<?= $this->Html->link('Add Article', ['action' => 'add']) ?>
 <table>
     <tr>
         <th>Title</th>
         <th>Created</th>
+        <th>Action</th>
+
     </tr>
 
     <!-- Here is where we iterate through our $articles query object, printing out article info -->
@@ -16,6 +19,14 @@
         </td>
         <td>
             <?= $article->created->format(DATE_RFC850) ?>
+        </td>
+        <td>
+            <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
+            <?= $this->Form->postLink(
+                'Delete',
+                ['action' => 'delete', $article->slug],
+                ['confirm' => 'Are you sure?'])
+            ?>
         </td>
     </tr>
     <?php endforeach; ?>
